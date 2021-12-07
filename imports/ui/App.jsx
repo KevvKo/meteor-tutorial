adcdd68b2a0ac2d1d8038ab8fa088d9d5e74eb87
KevvKo <kklein915@gmail.com>
2021-12-07 12:06:52 +0100

    add new components and styles

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,17 +1,46 @@
 import React from 'react';
+import { useTracker } from 'meteor/react-meteor-data';
+import { TasksCollection } from '../api/TaskCollection';
 import { Task } from './Task';
+import { TaskForm } from './TaskForm';
 
-const tasks = [
-  {_id: 1, text: 'First Task'},
-  {_id: 2, text: 'Second Task'},
-  {_id: 3, text: 'Third Task'},
-];
- 
-export const App = () => (
-  <div> 
-    <ul>
-      { tasks.map(task => <Task key={ task._id } task={ task }/>) }
-    </ul>
-  </div>
-);
+const toggleChecked = ({ _id, isChecked }) => {
+  TasksCollection.update(_id, {
+    $set: {
+      isChecked: !isChecked
+    }
+  })
+};
 
+const deleteTask = ({ _id }) => TasksCollection.remove(_id);
+
+export const App = () => {
+
+  const tasks = useTracker(() => TasksCollection.find({}, { sort: { createdAt: -1 } }).fetch()); 
+  return (
+    <div className="app">
+      <header>
+        <div className="app-bar">
+          <div className="app-header">
+            <h1>To Do-List</h1>
+          </div>
+        </div>
+      </header>
+
+      <div className="main">
+        <TaskForm />
+
+        <ul className="tasks">
+          {tasks.map(task => (
+            <Task
+              key={task._id}
+              task={task}
+              onCheckboxClick={toggleChecked}
+              onDeleteClick={deleteTask}
+            />
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
